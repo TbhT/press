6df6d42f44799ddb08e8ddec3b1e6f914852bffa
wangbinghua <binghua.wang@upchina.com>
2018-06-07 11:37:50 +0800

    update router

--- lib/Router/Router.php
+++ lib/Router/Router.php
@@ -11,6 +11,11 @@ use Press\Router\Route;
 use Press\Router\Layer;
 
 
+/**
+ * Class Router
+ * @package Press\Router
+ * @property $_params 用来自定义 param 方法
+ */
 class Router
 {
     private $_params = [];
@@ -31,7 +36,35 @@ class Router
 
     public function param(string $name, callable $fn)
     {
+        if (is_callable($name)) {
+            array_push($this->_params, $name);
+            return;
+        }
 
+        $length = count($this->_params);
+        $_name = substr($name, 0, 1);
+        if ($_name === ':') {
+            $name = $_name;
+        }
+
+//        用来自定义 router.param 方法, 只返回一个函数作为param的handle
+        foreach ($this->_params as $key => $customFn) {
+            if ($ret = $fn($name, $customFn)) {
+                $fn = $ret;
+            }
+        }
+
+        if (is_callable($fn) === false) {
+            throw new \Error("Invalid param() call for {$name}, got {$fn}");
+        }
+
+        if (is_array($this->params[$name]) === false) {
+            $this->params[$name] = [];
+        }
+
+        array_push($this->params[$name], $fn);
+
+        return $this;
     }
 
 
