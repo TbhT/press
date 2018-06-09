fa7fc6eb7b09012bebc0539b9c5cbfc0a4f64a61
wangbinghua <binghua.wang@upchina.com>
2018-06-09 18:28:37 +0800

    add Route test cases

---
+++ tests/Router/Route.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+use PHPUnit\Framework\TestCase;
+use Press\Router\Route;
+use Press\Tool\HttpHelper;
+
+
+class RouteTest extends TestCase
+{
+
+    public function testWithoutHandlers()
+    {
+        $req = ['method' => 'GET', 'url' => '/'];
+        $res = [];
+        $route = new Route('/foo');
+        $done = function () {
+            $this->assertTrue(true);
+        };
+        $route->dispatch($req, $res, $done);
+    }
+
+    public function testRouteAllMethod()
+    {
+        $req = ['method' => 'GET', 'url' => '/'];
+        $res = [];
+        $route = new Route('/foo');
+        $done = function ($error) use ($req) {
+            if ($error) {
+                $this->assertFalse(false);
+                return;
+            }
+
+            $this->assertTrue($req->called);
+        };
+
+
+        $route->all(function ($req, $res, $next) {
+            $req->called = true;
+            $next();
+        });
+
+
+        $route->dispatch($req, $res, $done);
+    }
+}
