1b368bfea4b7a7842a72bca8a478c7c7a766a6d4
TbhT <hustapple132@gmail.com>
2018-06-26 23:56:11 +0800

    add negotiator mediaType module

--- lib/Utils/Negotiator/MediaType.php
+++ lib/Utils/Negotiator/MediaType.php
@@ -25,6 +25,23 @@ function quote_count(string $str): int
 }
 
 
+function split_key_val_pair() {
+    return function (string $str) : array {
+        $index = strpos($str, '=');
+        $val = null;
+
+        if ($index === -1) {
+            $key = $str;
+        } else {
+            $key = substr($str, $index);
+            $val = substr($str, $index + 1);
+        }
+
+        return [$key, $val];
+    };
+}
+
+
 class MediaType
 {
     private static function parseAccept(string $accept)
@@ -66,6 +83,39 @@ class MediaType
 
     private static function parseMediaType(string $accept, int $i)
     {
+        preg_match('/^\s*([^\s\/;]+)\/([^;\s]+)\s*(?:;(.*))?$/', $accept, $matches);
+        $m_length = count($matches);
+        if ($m_length === 0) return null;
 
+        $params = [];
+        $q = 1;
+        $sub_type = $matches[2];
+        $type = $matches[1];
+
+        if ($matches[3]) {
+            $kvps = self::splitParameters($matches[3]);
+            array_map(split_key_val_pair(), $kvps);
+
+        }
+    }
+
+    private static function splitParameters(string $str)
+    {
+        $parameters = explode(';', $str);
+        for ($i = 0, $j = 0; $i < count($parameters); $i++) {
+            if (quote_count($parameters[$i]) % 2 === 0) {
+                $parameters[++$j] = $parameters[$i];
+            } else {
+                $parameters[$j] .= ';' . $parameters[$i];
+            }
+        }
+
+        $parameters['length'] = $j + 1;
+
+        for ($i = 0; $i < $parameters['length']; $i++) {
+            $parameters[$i] = trim($parameters[$i]);
+        }
+
+        return $parameters;
     }
 }
