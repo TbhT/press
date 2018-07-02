c99e4b028fe31bbfbbd15d8f018c76c2b741b985
TbhT <hustapple132@gmail.com>
2018-07-02 22:36:54 +0800

    update mime module

--- lib/Utils/Mime/MimeTypes.php
+++ lib/Utils/Mime/MimeTypes.php
@@ -10,13 +10,113 @@ declare(strict_types=1);
 namespace Press\Utils\Mime;
 
 
+function extname(string $str)
+{
+    $str_array = explode('.', $str);
+    $str_array_length = count($str_array);
+
+    if ($str_array_length > 0) {
+        return $str_array[$str_array_length - 1];
+    }
+
+    return '';
+}
+
+
 class MimeTypes
 {
-    private static function parseMimeDb(): \stdClass
+    private $preference = ['nginx', 'apache', null, 'iana'];
+
+    public static $extensions = [];
+    public static $types = [];
+
+    public static function populateMaps($extensions, $types)
     {
-        $db_path = dirname(__DIR__ . '/db.json');
-        return json_decode(file_get_contents($db_path));
+        foreach (array_keys(MIMEDB) as $type) {
+            $mime = MIMEDB[$type];
+            $exts = $mime['extensions'];
+
+            if (empty($exts) || count($exts) === 0) {
+                return null;
+            }
+
+            $extensions[$type] = $exts;
+
+            foreach ($exts as $extension) {
+                if ($types[$extension]) {
+
+                }
+            }
+        }
     }
 
 
+    public static function charset($type)
+    {
+        if (empty($type) || is_string($type) === false) {
+            return false;
+        }
+
+//        extract type regexp
+        preg_match('/^\s*([^;\s]*)(?:;|\s|$)/', $type, $matches);
+        $mime = $matches && strtolower(MIMEDB[$matches[1]]);
+
+        if ($mime && array_key_exists('charset', $mime)) {
+            return $mime['charset'];
+        }
+
+//        default text/* to utf-8
+        if (count($matches) > 0) {
+            preg_match('/^text\//', $matches[1], $text_matches);
+            if (count($text_matches) > 0) {
+                return 'UTF-8';
+            }
+        }
+
+        return false;
+    }
+
+
+    private static function contentType(string $str)
+    {
+        if (empty($str) || is_string($str) === false) {
+            return false;
+        }
+
+        $mime = strpos($str, '/') === false ? self::loopup($str) : $str;
+
+        if (empty($mime)) {
+            return false;
+        }
+
+//        todo: use content-type or other module
+        if (strpos($mime, 'charset') === false) {
+            $charset = self::charset($mime);
+            if ($mime) {
+                $mime .= '; charset=' . strtolower($charset);
+            }
+        }
+
+        return $mime;
+    }
+
+
+    public function lookup(string $path)
+    {
+        if (empty($path) || is_string($path) === false) {
+            return false;
+        }
+
+        $extension = strtolower(extname('x.' . $path));
+        $extension = substr($extension, 1);
+
+        if (empty($extension)) {
+            return false;
+        }
+
+        return self::$types[$extension] || false;
+    }
+
 }
+
+
