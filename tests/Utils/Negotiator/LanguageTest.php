c51d797b5e4a4f423e1349d5d35999536b11479c
TbhT <hustapple132@gmail.com>
2018-07-12 23:49:17 +0800

    add language and media types

---
+++ tests/Utils/Negotiator/LanguageTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-7-12
+ * Time: 下午11:12
+ */
+
+use Press\Utils\Negotiator\Language;
+use PHPUnit\Framework\TestCase;
+
+class LanguageTest extends TestCase
+{
+
+}
---
+++ tests/Utils/Negotiator/MediaTypeTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-7-12
+ * Time: 下午11:12
+ */
+
+use Press\Utils\Negotiator\MediaType;
+use PHPUnit\Framework\TestCase;
+
+class MediaTypeTest extends TestCase
+{
+
+}
