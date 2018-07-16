3d143c0ba03a4af92db7a33c415b59d99e8f90c5
TbhT <hustapple132@gmail.com>
2018-07-16 09:15:38 +0800

    add accepts unit tests

--- tests/Utils/AcceptsTest.php
+++ tests/Utils/AcceptsTest.php
@@ -13,26 +13,104 @@ use PHPUnit\Framework\TestCase;
 
 class AcceptsTest extends TestCase
 {
-    private static function createRequest(string $str)
+    private static function createRequestCharset($charset = null)
     {
         $req = new Request();
         $req->headers = [
-            'accept-charset' => $str
+            'accept-charset' => $charset
         ];
 
         return $req;
     }
 
-    public function testCharsetWithNoArguments()
+    private static function createRequestEncoding($encoding = null)
     {
-        {
-            $req = self::createRequest('utf-8, iso-8859-1;q=0.2, utf-7;q=0.5');
-            $accept = new Accepts($req);
-            $charsets = $accept->charsets();
-            self::assertEquals($charsets, ['utf-8', 'utf-7', 'iso-8859-1']);
-        };
-        {
+        $req = new Request();
+        $req->headers = [
+            'accept-encoding' => $encoding
+        ];
 
-        }
+        return $req;
+    }
+
+    private static function createRequestLanguage($language = null)
+    {
+        $req = new Request();
+        $req->headers = [
+            'accept-language' => $language
+        ];
+
+        return $req;
+    }
+
+    private static function createRequestType($type = null)
+    {
+        $req = new Request();
+        $req->headers = [
+            'accept' => $type
+        ];
+
+        return $req;
+    }
+
+
+    /**
+     * accept-headers, expected
+     * @return array
+     */
+    public function charsetWithNoArgumentsData()
+    {
+        return [
+            [
+                'utf-8, iso-8859-1;q=0.2, utf-7;q=0.5',
+                ['utf-8', 'utf-7', 'iso-8859-1']
+            ],
+            [
+                null, ['*']
+            ],
+            [
+                '', []
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider charsetWithNoArgumentsData
+     * @param $accept_header
+     * @param $expected
+     */
+    public function testCharsetWithNoArguments($accept_header, $expected)
+    {
+        $req = self::createRequestCharset($accept_header);
+        $accept = new Accepts($req);
+        $charsets = $accept->charsets();
+        self::assertEquals($expected, $charsets);
+    }
+
+    public function testCharsetWithMultipleArguments()
+    {
+        $req1 = self::createRequestCharset('utf-8, iso-8859-1;q=0.2, utf-7;q=0.5');
+        $accept1 = new Accepts($req1);
+        $charsets1 = $accept1->charsets('utf-7', 'utf-8');
+        self::assertEquals(['utf-8'], $charsets1);
+
+
+        $req2 = self::createRequestCharset('utf-8, iso-8859-1;q=0.2, utf-7;q=0.5');
+        $accept2 = new Accepts($req2);
+        $charsets2 = $accept2->charsets('utf-16');
+        self::assertEquals(false, $charsets2);
+
+        $req3 = self::createRequestCharset();
+        $accept3 = new Accepts($req3);
+        $charsets3 = $accept3->charsets('utf-7', 'utf-8');
+        self::assertEquals('utf-7', $charsets3);
+    }
+
+    public function testCharsetWithArray()
+    {
+        $req = self::createRequestCharset('utf-8, iso-8859-1;q=0.2, utf-7;q=0.5');
+        $accept = new Accepts($req);
+        $charsets = $accept->charsets(['utf-7', 'utf-8']);
+        self::assertEquals('utf-8', $charsets);
     }
 }
