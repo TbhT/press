6473034d0063232e6cc363d5cb3bb26afab9c634
TbhT <hustapple132@gmail.com>
2018-08-07 20:52:19 +0800

    add forwarded module units test

---
+++ tests/Utils/ForwardedTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 88002
+ * Date: 2018-08-07
+ * Time: 20:38
+ */
+
+use Press\Utils\Forwarded;
+use Press\Request;
+use PHPUnit\Framework\TestCase;
+
+
+class ForwardedTest extends TestCase
+{
+    private function createReq($socket_addr, $headers = null)
+    {
+        if (count($headers) === 0) {
+            $headers = [];
+        }
+
+        $req = new Request();
+        $req->server['remote_addr'] = $socket_addr;
+        $req->headers = $headers;
+
+        return $req;
+    }
+
+
+    /**
+     * @expectedException TypeError
+     */
+    public function testWithoutReq()
+    {
+        Forwarded::forwarded();
+    }
+
+
+    public function testWithXHeader()
+    {
+        $req = self::createReq('127.0.0.1');
+        self::assertEquals(['127.0.0.1'], Forwarded::forwarded($req));
+    }
+
+
+    public function testShouldIncludeEntriesFromXHeader()
+    {
+        $req = self::createReq('127.0.0.1', [
+            'x-forwarded-for' => '10.0.0.2, 10.0.0.1'
+        ]);
+
+        self::assertEquals(['127.0.0.1', '10.0.0.1', '10.0.0.2'], Forwarded::forwarded($req));
+    }
+
+
+    public function testShouldSkipBlankEntries()
+    {
+        $req = self::createReq('127.0.0.1', [
+            'x-forwarded-for' => '10.0.0.2,,10.0.0.1'
+        ]);
+
+        self::assertEquals(['127.0.0.1', '10.0.0.1', '10.0.0.2'], Forwarded::forwarded($req));
+    }
+
+
+    public function testShouldTrimLeadingOWS()
+    {
+        $req = self::createReq('127.0.0.1', [
+            'x-forwarded-for' => '10.0.0.2, ,10.0.0.1'
+        ]);
+
+        self::assertEquals(['127.0.0.1', '10.0.0.1', '10.0.0.2'], Forwarded::forwarded($req));
+    }
+
+}
