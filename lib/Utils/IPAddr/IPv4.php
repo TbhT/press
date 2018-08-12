cfa73393c063a12dd8bb9840b69d21ecaed1ef37
TbhT <hustapple132@gmail.com>
2018-08-13 00:41:38 +0800

    add ipaddr unit tests

---
+++ lib/Utils/IPAddr/IPv4.php
@@ -0,0 +1,306 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-8-12
+ * Time: 下午4:57
+ */
+
+namespace Press\Utils\IPAddr;
+
+
+const IPV4_FOUR_OCTET = '/^(0?\d+|0x[a-f0-9]+)\.(0?\d+|0x[a-f0-9]+)\.(0?\d+|0x[a-f0-9]+)\.(0?\d+|0x[a-f0-9]+)$/i';
+
+const IPV4_LONG_VALUE = '/^(0?\d+|0x[a-f0-9]+)$/i';
+
+
+class IPv4
+{
+    public $octets;
+    private $specialRangesArray = [
+        'unspecified' => [],
+        'broadcast' => [],
+        'multicast' => [],
+        'linkLocal' => [],
+        'loopback' => [],
+        'carrierGradeNat' => [],
+        'private' => [],
+        'reserved' => []
+    ];
+
+    public function __construct(array $octets)
+    {
+        if (count($octets) !== 4) {
+            throw new \TypeError('IpAddr: IPv4 octet count should be 4');
+        }
+
+        foreach ($octets as $octet) {
+            if (!(0 <= $octet && $octet <= 255)) {
+                throw new \TypeError('IpAddr: IPv4 octet should fit in 8 bits');
+            }
+        }
+
+        $this->octets = $octets;
+    }
+
+
+    public function kind()
+    {
+        return 'ipv4';
+    }
+
+
+    public function toString()
+    {
+        return join('.', $this->octets);
+    }
+
+
+    public function toByteArray()
+    {
+        return $this->octets;
+    }
+
+
+    public function match($other, $cidrRange = null)
+    {
+        if ($cidrRange === null) {
+            $cidrRange = $other[1];
+            $other = $other[0];
+        }
+
+        if ($other->kind() !== 'ipv4') {
+            throw new \TypeError('IpAddr: cannot match ipv4 address with non-ipv4 one');
+        }
+
+        return Tool::matchCIDR($this->octets, $other->octets, 8, $cidrRange);
+    }
+
+
+    public function range()
+    {
+        return Tool::subnetMatch($this, $this->specialRanges);
+    }
+
+
+    public function toIPv4MappedAddress()
+    {
+        $str = $this->toString();
+        return IPv6::parse("::ffff:{$str}");
+    }
+
+
+    public function prefixLengthFromSubnetMask()
+    {
+        $zeroTable = [
+            '0' => 8,
+            '128' => 7,
+            '192' => 6,
+            '224' => 5,
+            '240' => 4,
+            '248' => 3,
+            '252' => 2,
+            '254' => 1,
+            '255' => 0
+        ];
+
+        $cidr = 0;
+        $stop = false;
+
+        for ($i = 3; $i >= 0; $i--) {
+            $octet = $this->octets[$i];
+            if (array_key_exists($octet, $zeroTable)) {
+                $zeros = $zeroTable[$octet];
+                if ($stop && $zeros !== 0) {
+                    return null;
+                }
+
+                if ($zeros !== 8) {
+                    $stop = true;
+                }
+
+                $cidr += $zeros;
+            } else {
+                return null;
+            }
+        }
+
+        return 32 - $cidr;
+    }
+
+
+    public static function parser(string $string)
+    {
+        preg_match(IPV4_FOUR_OCTET, $string, $match_four);
+        preg_match(IPV4_LONG_VALUE, $string, $match_long);
+
+        if (count($match_four) > 0) {
+            $result = [];
+            for ($i = 1; $i < count($match_four); $i++) {
+                $part = $match_four[$i];
+                array_push($result, self::parseIntAuto($part));
+            }
+
+            return $result;
+        } elseif (count($match_long) > 0) {
+            $value = self::parseIntAuto($match_long[1]);
+            if ($value > 0xffffffff || $value < 0) {
+                throw new \TypeError('IpAddr: address outside defined range');
+            }
+            $result = [];
+            for ($shift = $k = 0; $k <= 24; $shift = $k += 8) {
+                array_push($result, ($value >> $shift) & 0xff);
+            }
+
+            return array_reverse($result);
+        } else {
+            return null;
+        }
+    }
+
+
+    private static function parseIntAuto(string $string)
+    {
+        return strval(intval($string, 0));
+    }
+
+
+    public static function parse(string $string)
+    {
+        $parts = static::parser($string);
+        if ($parts === null) {
+            throw new \TypeError('IpAddr: string is not formatted like ip address');
+        }
+
+        return new IPv4($parts);
+    }
+
+
+    public static function isIPv4(string $string)
+    {
+        return static::parser($string) !== null;
+    }
+
+
+    public static function isValid(string $str)
+    {
+        try {
+            new IPv4(static::parser($str));
+            return true;
+        } catch (\Throwable $exception) {
+            return false;
+        }
+    }
+
+
+    public function isValidFourPartDecimal(string $str)
+    {
+        preg_match('/^\d+(\.\d+){3}$/', $str, $m);
+        if (static::isIPv4($str) && count($m) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static function parseCIDR(string $string)
+    {
+        preg_match('/^(.+)\/(\d+)$/', $string, $m);
+        if (count($m) > 0) {
+            $maskLength = intval($m[2]);
+            if ($maskLength >= 0 && $maskLength <= 32) {
+                return [static::parse($m[1]), $maskLength];
+            }
+        }
+
+        throw new \TypeError('IpAddr: string is not formatted like an IPv4 CIDR range');
+    }
+
+
+    public function broadcastAddressFromCIDR(string $str)
+    {
+        try {
+            $cidrValue = IPv4::parseCIDR($str);
+            $ipInterface = $cidrValue[0];
+            $subnetMask = IPv4::subnetMaskFromPrefixLength($cidrValue[1]);
+            $octets = [];
+            $i = 0;
+            while ($i < 4) {
+                array_push($octets, intval($ipInterface->octets[$i], 10) | intval($subnetMask[$i], 10) ^ 255);
+                $i++;
+            }
+
+            return new IPv4($octets);
+        } catch (\Throwable $exception) {
+            throw new \TypeError('IpAddr: the address does not have ipv4 CIDR format');
+        }
+    }
+
+
+    public static function subnetMaskFromPrefixLength($prefix)
+    {
+        if ($prefix < 0 || $prefix > 32) {
+            throw new \TypeError('IpAddr: invalid prefix length');
+        }
+
+        $octets = [0, 0, 0, 0];
+        $j = 0;
+        while ($j < floor($prefix / 8)) {
+            $octets[$j] = 255;
+            $j++;
+        }
+
+        $f_val = floor($prefix / 8);
+        $p_val = pow(2, $prefix % 8);
+        $octets[$f_val] = $p_val - 1 << 8 - ($prefix % 8);
+
+        return new IPv4($octets);
+    }
+
+
+    public function __get($name)
+    {
+        if ($name === 'specialRanges') {
+            $this->specialRanges();
+            return $this->specialRangesArray;
+        }
+    }
+
+
+    private function specialRanges()
+    {
+        $this->specialRangesArray['unspecified'] = [
+            [(new IPv4([0, 0, 0, 0])), 8]
+        ];
+        $this->specialRangesArray['broadcast'] = [
+            [(new IPv4([255, 255, 255, 255])), 32]
+        ];
+        $this->specialRangesArray['multicast'] = [
+            [(new IPv4([224, 0, 0, 0])), 4]
+        ];
+        $this->specialRangesArray['linkLocal'] = [
+            [(new IPv4([169, 254, 0, 0])), 16]
+        ];
+        $this->specialRangesArray['loopback'] = [
+            [(new IPv4([127, 0, 0, 0])), 8]
+        ];
+        $this->specialRangesArray['carrierGradeNat'] = [
+            [(new IPv4([100, 64, 0, 0])), 10]
+        ];
+        $this->specialRangesArray['private'] = [
+            [(new IPv4([10, 0, 0, 0])), 8],
+            [(new IPv4([172, 16, 0, 0,])), 12],
+            [(new IPv4([192, 168, 0, 0])), 16]
+        ];
+        $this->specialRangesArray['reserved'] = [
+            [(new IPv4([192, 0, 0, 0])), 24],
+            [(new IPv4([192, 0, 2, 0])), 24],
+            [(new IPv4([192, 88, 99, 0])), 24],
+            [(new IPv4([198, 51, 100, 0])), 24],
+            [(new IPv4([203, 0, 113, 0])), 24],
+            [(new IPv4([240, 0, 0, 0])), 4]
+        ];
+    }
+}
---
+++ lib/Utils/IPAddr/Tool.php
@@ -0,0 +1,161 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-8-8
+ * Time: 下午11:21
+ */
+
+namespace Press\Utils\IPAddr;
+
+
+class Tool
+{
+    public static function isValid(string $str)
+    {
+        return IPv6::isValid($str) || IPv4::isValid($str);
+    }
+
+
+    public static function parse(string $str)
+    {
+        if (IPv6::isValid($str)) {
+            return IPv6::parse($str);
+        } else if (IPv4::isValid($str)) {
+            return IPv4::parse($str);
+        } else {
+            throw new \TypeError('IpAddr: the address has neither IPv6 nor IPv4 format');
+        }
+    }
+
+
+    public static function parseCIDR(string $str)
+    {
+        try {
+            return IPv6::parseCIDR($str);
+        } catch (\Throwable $e1) {
+            try {
+                return IPv4::parseCIDR($str);
+            } catch (\Throwable $e2) {
+                throw new \TypeError('IpAddr: the address has neither IPv6 nor IPv4 CIDR format');
+            }
+        }
+    }
+
+
+    public static function fromByteArray($bytes)
+    {
+        $length = count($bytes);
+        if ($length === 4) {
+            return new IPv4($bytes);
+        } else if ($length === 16) {
+            return new IPv6($bytes);
+        } else {
+            throw new \TypeError('IpAddr: the binary input is neither an IPv6 nor IPv4 address');
+        }
+    }
+
+
+    public static function process(string $str)
+    {
+        $addr = static::parse($str);
+        if ($addr->kind() === 'ipv6' && $addr->isIPv4MappedAddress()) {
+            return $addr->toIPv4Address();
+        } else {
+            return $addr;
+        }
+    }
+
+    public static function matchCIDR(array $first, array $second, $partSize, $cidrBits)
+    {
+        if (count($first) !== count($second)) {
+            throw new \TypeError('IpAddr: cannot match CIDR for arrays with different lengths');
+        }
+
+        $part = 0;
+        while ($cidrBits > 0) {
+            $shift = $partSize - $cidrBits;
+            if ($shift < 0) {
+                $shift = 0;
+            }
+
+            if ($first[$part] >> $shift !== $second[$part] >> $shift) {
+                return false;
+            }
+
+            $cidrBits -= $partSize;
+            $part += 1;
+        }
+
+        return true;
+    }
+
+
+    public static function subnetMatch($address, array $rangeList, $defaultName = 'unicast')
+    {
+        foreach ($rangeList as $rangeName => $rangeSubnets) {
+            if ($rangeSubnets[0] && is_array($rangeSubnets[0]) === false) {
+                $rangeSubnets = [$rangeSubnets];
+            }
+
+            foreach ($rangeSubnets as $rangeSubnet) {
+                if ($address->match($rangeSubnet)) {
+                    return $rangeName;
+                }
+            }
+        }
+
+        return $defaultName;
+    }
+
+
+    public static function expandIPv6(string $string, $parts)
+    {
+        if (strpos($string, '::') !== stripos($string, '::')) {
+            return null;
+        }
+
+        $colonCount = 0;
+        $lastColon = -1;
+
+        while (($lastColon = strpos($string, ':', $lastColon + 1)) >= 0) {
+            $colonCount++;
+        }
+
+        if (substr($string, 0, 2) === '::') {
+            $colonCount--;
+        }
+
+        if (substr($string, -2, 2) === '::') {
+            $colonCount--;
+        }
+
+        if ($colonCount > $parts) {
+            return null;
+        }
+
+        $replacementCount = $parts - $colonCount;
+        $replacement = '';
+
+        while ($replacementCount--) $replacement .= '0:';
+
+        $string = str_replace('::', $replacement, $string);
+        if ($string[0] === ':') {
+            $string = substr($string, 1);
+        }
+
+        if ($string[strlen($string) - 1] === ':') {
+            $string = substr($string, 0, -1);
+        }
+
+        $strs = explode(':', $string);
+        $result = [];
+        foreach ($strs as $str) {
+            array_push($result, intval($str, 16));
+        }
+
+        return $result;
+    }
+
+}
