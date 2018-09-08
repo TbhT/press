c2b742ea8864329a9eb0a738b3303e924149d902
TbhT <hustapple132@gmail.com>
2018-09-08 10:36:45 +0800

    add events lib

---
+++ lib/Utils/Events.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-9-6
+ * Time: 下午10:32
+ */
+
+namespace Press\Utils;
+
+
+class Events
+{
+    public $name;
+
+    public $sender;
+}
