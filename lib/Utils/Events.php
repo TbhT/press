723d039252b6472fd682be9ea703d6d962023f89
TbhT <hustapple132@gmail.com>
2018-09-11 23:32:28 +0800

    update the events module

--- lib/Utils/Events.php
+++ lib/Utils/Events.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 namespace Press\Utils;
 
 
-use PHPUnit\Util\Type;
 
 class Events
 {
@@ -19,12 +18,15 @@ class Events
 
     /**
      *  Finds the index of the listener for the event in its storage array.
+     * @param $listeners
+     * @param $listener
+     * @return int
      */
     private function index_of_listener($listeners, $listener)
     {
         $length = count($listeners);
         while ($length--) {
-            if ($listeners[$length]->listener === $listener) {
+            if ($listeners[$length]['listener'] === $listener) {
                 return $length;
             }
         }
@@ -32,23 +34,24 @@ class Events
         return -1;
     }
 
+    /**
+     * @return array
+     */
     private function get_events()
     {
         return $this->events;
     }
 
+    /**
+     * @param $listener
+     * @return bool
+     */
     private function is_valid_listener($listener)
     {
         if (is_callable($listener)) {
             return true;
-        } else if (is_array($listener)) {
-            $flag = true;
-            foreach ($listener as $item) {
-                if (is_callable($item)) {
-                    $flag = false;
-                }
-            }
-            return $flag;
+        } else if (is_array($listener) && array_key_exists('listener', $listener)) {
+            return $this->is_valid_listener($listener['listener']);
         } else {
             return false;
         }
@@ -64,6 +67,8 @@ class Events
      * Will initialise the event object and listener arrays if required.
      * Will return an object if you use a regex search. The object contains keys for each matched event. So /ba[rz]/ might return an object containing bar and baz. But only if you have either defined them with defineEvent or added some listeners to them.
      * Each property in the object response is an array of listener functions.
+     * @param string $event
+     * @return array|mixed
      */
     public function get_listeners(string $event)
     {
@@ -80,6 +85,29 @@ class Events
             }
         } else if (array_key_exists($event, $events)) {
             $response = $events[$event];
+        } else {
+            $events[$event] = [];
+        }
+
+        // update events
+        $this->events = $events;
+
+        return $response;
+    }
+
+    /**
+     * @param string $evt
+     * @return array|mixed
+     */
+    public function get_listeners_wrapper(string $evt)
+    {
+        $listeners = $this->get_listeners($evt);
+
+        if (is_array($listeners)) {
+            $response = [];
+            $response[$evt] = $listeners;
+        } else {
+            $response = $listeners;
         }
 
         return $response;
@@ -87,6 +115,8 @@ class Events
 
     /**
      * Takes a list of listener objects and flattens it into a list of listener functions.
+     * @param array $listeners
+     * @return array
      */
     public function flatten_listeners(array $listeners)
     {
@@ -113,32 +143,71 @@ class Events
             throw new \TypeError('listener must be function');
         }
 
-        $listeners = $this->get_listeners($evt);
+        $listeners = $this->get_listeners_wrapper($evt);
+
+        foreach ($listeners as $key => $value) {
+            if ($this->index_of_listener($value, $listener) === -1) {
+                if (is_array($listener)) {
+                    array_push($value, $listener);
+                } else {
+                    $l = ['listener' => $listener, 'once' => false];
+                    array_push($value, $l);
+                }
+            }
+        }
+
+        return $this;
     }
 
-    public function on()
+    public function on($evt, $listener)
     {
-        return $this->add_listener();
+        return $this->add_listener($evt, $listener);
     }
 
-    public function add_once_listener()
+    /**
+     * @param $evt
+     * @param $listener
+     * @return Events
+     */
+    public function add_once_listener($evt, $listener)
     {
-
+        return $this->add_listener($evt, [
+            'listener' => $listener,
+            'once' => true
+        ]);
     }
 
-    public function once()
+    /**
+     * @param $evt
+     * @param $listener
+     * @return Events
+     */
+    public function once($evt, $listener)
     {
-        return $this->add_once_listener();
+        return $this->add_once_listener($evt, $listener);
     }
 
-    public function define_event()
+    /**
+     * @param string $event
+     * @return Events
+     */
+    public function define_event(string $event)
     {
-
+        $this->get_listeners($event);
+        return $this;
     }
 
-    public function define_events()
+    /**
+     * @param $evts
+     * @return Events
+     */
+    public function define_events($evts)
     {
+        foreach ($evts as $evt) {
+            $this->define_event($evt);
+        }
 
+        return $this;
     }
 
     public function remove_listener()
