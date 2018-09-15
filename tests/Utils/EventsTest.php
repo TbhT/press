7bd2bc918d53490429857ebf90f63b779afdfd48
TbhT <hustapple132@gmail.com>
2018-09-16 00:23:45 +0800

    update dependencies

---
+++ tests/Utils/EventsTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: erub
+ * Date: 18-9-15
+ * Time: 下午11:54
+ */
+
+use Press\Utils\Events;
+use PHPUnit\Framework\TestCase;
+
+class EventsTest extends TestCase
+{
+
+}
