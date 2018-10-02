d6eaf18b8549cff884f913df09bb411a83e9e10d
TbhT <hustapple132@gmail.com>
2018-10-02 22:15:17 +0800

    add content type module

--- lib/Utils/ContentType.php
+++ lib/Utils/ContentType.php
@@ -19,10 +19,44 @@ namespace Press\Utils;
  */
 
 const PARAM_REG_EXP = '/; *([!#$%&\'*+.^_`|~0-9A-Za-z-]+) *= *("(?:[\x{000b}\x{0020}\x{0021}\x{0023}-\x{005b}\x{005d}-\x{007e}\x{0080}-\x{00ff}]|\\[\x{000b}\x{0020}-\x{00ff}])*"|[!#$%&\'*+.^_`|~0-9A-Za-z-]+) */u';
-const TEXT_REG_EXP = '';
+const TEXT_REG_EXP = '/^[\x{000b}\x{0020}-\x{007e}\x{0080}-\x{00ff}]+$/u';
+const TOKEN_REG_EXP = '/^[!#$%&\'*+.^_`|~0-9A-Za-z-]+$/u';
+
+const QESC_REG_EXP = '/\\([\u000b\u0020-\u00ff])/';
+
+const QUOTE_REG_EXP = '/([\\"])/';
+
+const TYPE_REG_EXP = '/^[!#$%&\'*+.^_`|~0-9A-Za-z-]+\/[!#$%&\'*+.^_`|~0-9A-Za-z-]+$/';
 
 
 class ContentType
 {
+    public static function format(array $t)
+    {
+        $t = empty($t) ? [
+            'parameters' => null,
+            'type' => ''
+        ] : $t;
+
+        $parameters = $t['parameters'];
+        $type = $t['type'];
+
+        preg_match(TYPE_REG_EXP, $type,$m);
+
+        if (!$type || count($m) === 0) {
+            throw new \TypeError('invalid type');
+        }
+
+        // append parameters
+        if (!empty($parameters) && gettype($parameters) === 'array') {
+
+        }
+    }
+
+    public static function parse()
+    {
+
+    }
+
 
 }
