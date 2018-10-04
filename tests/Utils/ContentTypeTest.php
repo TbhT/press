b538baaf39092bf4a13a3321a510a5399c7bad32
TbhT <hustapple132@gmail.com>
2018-10-04 15:28:54 +0800

    update content type unit test

--- tests/Utils/ContentTypeTest.php
+++ tests/Utils/ContentTypeTest.php
@@ -5,6 +5,24 @@ declare(strict_types=1);
 use Press\Utils\ContentType;
 use PHPUnit\Framework\TestCase;
 
+
+/**
+ * @param $a
+ * @param $b
+ * @return bool
+ */
+function arrays_are_similar($a, $b)
+{
+    foreach ($a as $k => $v) {
+        if ($v !== $b[$k]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 class ContentTypeTest extends TestCase
 {
     public function formatData()
@@ -46,5 +64,98 @@ class ContentTypeTest extends TestCase
     {
         $str = ContentType::format($format);
         self::assertEquals($expect, $str);
+    }
+
+    /**
+     * @return array
+     */
+    public function throwFormatData()
+    {
+        return [
+            [null],
+            [7],
+            [[]],
+            [['type' => 'text/']],
+            [['type' => ' text/html']],
+            [['type' => 'image/svg', 'parameters' => ['foo/' => 'bar']]],
+            [['type' => 'image/svg', 'parameters' => ['foo' => "bar\u{0000}"]]]
+        ];
+    }
+
+    /**
+     * @dataProvider throwFormatData
+     * @expectedException TypeError
+     */
+    public function testThrowFormatData($param)
+    {
+        ContentType::format($param);
+        self::assertTrue(true);
+    }
+
+    /**
+     * @return array
+     */
+    public function throwParseData()
+    {
+        return [
+            [' '],
+            ['null'],
+            ['undefined'],
+            ['/'],
+            ['text / plain'],
+            ['text/;plain'],
+            ['text/"plain"'],
+            ['text/p£ain'],
+            ['text/(plain)'],
+            ['text/@plain'],
+            ['text/plain,wrong']
+        ];
+    }
+
+    public function parseData()
+    {
+        return [
+            ['text/html', 'text/html'],
+            ['image/svg+xml', 'image/svg+xml'],
+            [' text/html ', 'text/html'],
+            ['text/html; charset=utf-8; foo=bar', 'text/html', [
+                'charset' => 'utf-8',
+                'foo' => 'bar'
+            ]],
+            ['text/html ; charset=utf-8 ; foo=bar', 'text/html', [
+                'charset' => 'utf-8',
+                'foo' => 'bar'
+            ]],
+            ['IMAGE/SVG+XML', 'image/svg+xml'],
+            ['text/html; Charset=UTF-8', 'text/html', [
+                'charset' => 'utf-8'
+            ]],
+            ['text/html; charset="UTF-8"', 'text/html', [
+                'charset' => 'UTF-8'
+            ]],
+            ['text/html; charset = "UT\\F-\\\\\\"8\\""', 'text/html', [
+                'charset' => 'UTF-\\"8"'
+            ]],
+            ['text/html; param="charset=\\"utf-8\\"; foo=bar"; bar=foo', 'text/html', [
+                'param' => 'charset="utf-8"; foo=bar',
+                'bar' => 'foo'
+            ]]
+        ];
+    }
+
+    /**
+     * @dataProvider parseData
+     * @param $src
+     * @param $expect1
+     * @param null $expect2
+     */
+    public function testParseData($src, $expect1, $expect2 = null)
+    {
+        $type = ContentType::parse($src);
+        self::assertEquals($expect1, $type['type']);
+
+        if (!empty($expect2)) {
+            self::assertTrue(arrays_are_similar($expect2, $type['parameters']));
+        }
     }
 }
