6df827b09af64bee2f5c26385faff6c3ef7e853f
TbhT <hustapple132@gmail.com>
2018-10-15 13:25:38 +0800

    update view

--- lib/Helper/FunctionHelper.php
+++ lib/Helper/FunctionHelper.php
@@ -11,4 +11,25 @@ class FunctionHelper
     {
 
     }
+
+    static public function extname(string $str)
+    {
+        preg_match('/(^\.\w+$)|([\w\d.:\/\\\\]+)/', $str, $matches);
+        $matches_length = count($matches);
+
+        if ($matches_length === 2) {
+    //    for extension
+            $extname = substr($str, 1);
+        } else if ($matches_length === 3) {
+    //    for path
+            $file_info = pathinfo($matches[2]);
+            $extname = array_key_exists('extension', $file_info) ?
+                $file_info['filename'] === '' ? '' : $file_info['extension'] : '';
+        } else {
+            $extname = '';
+        }
+
+        return $extname;
+    }
+
 }
--- lib/View.php
+++ lib/View.php
@@ -3,7 +3,61 @@
 namespace Press;
 
 
+use Press\Helper\FunctionHelper;
+
+
 class View
 {
+    public function __construct(string $name,array $options = [])
+    {
+        $this->default_engine = array_key_exists('defaultEngine', $options) ? $options['defaultEngine'] : null;
+        $this->ext = FunctionHelper::extname($name);
+        $this->name = $name;
+        $this->root = array_key_exists('root', $options) ? $options['root'] : null;
 
+        if (!$this->root || !$this->default_engine) {
+            throw new \TypeError('root or defaultEngine must not be empty');
+        }
+
+
+    }
+
+    public function loopup(string $name)
+    {
+        $roots = array_merge([], $this->root);
+
+        foreach ($roots as $root) {
+            $location = stream_resolve_include_path("{$root}/{$name}");
+            $dir = dirname($location);
+            $file = basename($location);
+
+            $path = $this->resolve($dir, $file);
+        }
+
+        return $path;
+    }
+
+    public function render(array $options,callable $callback)
+    {
+        $this->engine($this->path, $options, $callback);
+    }
+
+    public function resolve(string $dir,string $file)
+    {
+        $path = join('.', [$dir, $file]);
+        $stat =  self::try_stat($path);
+
+        if (!empty($stat) && $stat['']) {
+            //TODO: 
+        }
+    }
+
+    private static function try_stat($path)
+    {
+        try {
+            return stat($path);
+        } catch (Throwable $e) {
+            return null;
+        }
+    }
 }
