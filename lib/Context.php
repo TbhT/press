f417a704adf042c9250d080c03bf03bfd0e485f9
TbhT <hustapple132@gmail.com>
2020-07-06 12:44:09 +0800

    feat: add context

--- lib/Context.php
+++ lib/Context.php
@@ -4,6 +4,7 @@
 namespace Press;
 
 
+use Press\Utils\Delegates;
 use Psr\Http\Message\ServerRequestInterface;
 use stdClass;
 
@@ -26,9 +27,59 @@ class Context
     public function __construct()
     {
         $this->state = new stdClass();
+        $res_delegates = new Delegates($this, 'response');
+        $res_delegates
+            ->method('attachment')
+            ->method('redirect')
+            ->method('remove')
+            ->method('vary')
+            ->method('has')
+            ->method('set')
+            ->method('append')
+            ->method('flushHeaders')
+            ->access('status')
+            ->access('message')
+            ->access('body')
+            ->access('length')
+            ->access('type')
+            ->access('lastModified')
+            ->access('etag')
+            ->getter('headerSent')
+            ->getter('writable');
+        $req_delegates = new Delegates($this, 'request');
+        $req_delegates->method('acceptsLanguages')
+            ->method('acceptsEncodings')
+            ->method('acceptsCharsets')
+            ->method('accepts')
+            ->method('get')
+            ->method('is')
+            ->access('querystring')
+            ->access('idempotent')
+            ->access('socket')
+            ->access('search')
+            ->access('method')
+            ->access('query')
+            ->access('path')
+            ->access('url')
+            ->access('accept')
+            ->getter('origin')
+            ->getter('href')
+            ->getter('subdomains')
+            ->getter('protocol')
+            ->getter('host')
+            ->getter('hostname')
+            ->getter('URL')
+            ->getter('header')
+            ->getter('headers')
+            ->getter('secure')
+            ->getter('stale')
+            ->getter('fresh')
+            ->getter('ips')
+            ->getter('ip');
     }
 
-    public function onerror() {
+    public function onerror()
+    {
         return function ($error) {
             var_dump('---error-----', $error);
         };
