08aa36022c909e5b59ed00a541a2279b21c5b1fa
TbhT <hustapple132@gmail.com>
2020-07-06 14:03:03 +0800

    feat: add request

--- lib/Request.php
+++ lib/Request.php
@@ -5,6 +5,7 @@ namespace Press;
 
 
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
 
 class Request
 {
@@ -19,4 +20,79 @@ class Request
     public ?Application $app = null;
 
     public string $originalUrl = '';
+
+    public function __get($name)
+    {
+        if ($name === 'header') {
+            return $this->getHeader();
+        }
+
+        return null;
+    }
+
+    public function __set($name, $value)
+    {
+        if ($name === 'header') {
+            $this->setHeader($name, $value);
+        }
+    }
+
+    private function getHeader()
+    {
+        return $this->req->getHeaders();
+    }
+
+    private function setHeader($name, $value)
+    {
+        $this->req->withHeader($name, $value);
+    }
+
+    private function getUrl()
+    {
+//        todo: url string whole
+    }
+
+    private function setUrl(UriInterface $url)
+    {
+//        todo: url string whole
+    }
+
+    private function getOrigin()
+    {
+//        todo: protocol . host
+    }
+
+    private function getHref()
+    {
+//        todo: protocol . host . url
+    }
+
+    private function getMethod(): string
+    {
+        return $this->req->getMethod();
+    }
+
+    private function setMethod($method)
+    {
+        $this->req->withMethod($method);
+    }
+
+    private function getHost(): string
+    {
+        $uri = $this->req->getUri();
+        return $uri->getHost();
+    }
+
+    private function getPath()
+    {
+        $uri = $this->req->getUri();
+        return $uri->getPath();
+    }
+
+    private function setPath(string $path)
+    {
+        $uri = $this->req->getUri();
+        $uri->withPath($path);
+    }
+
 }
