2afc4c604899b86cd09da62bc6110dd2e7cb8eb7
TbhT <hustapple132@gmail.com>
2020-07-06 17:43:25 +0800

    feat: change application logic

--- index.php
+++ index.php
@@ -3,3 +3,12 @@ declare(strict_types=1);
 
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new \Press\Application();
+
+$app->use(function (\Press\Context $ctx,callable $next) {
+    var_dump('>>>>>>hello world');
+    return $next();
+});
+
+$app->listen();
--- lib/Respond.php
+++ lib/Respond.php
@@ -4,7 +4,26 @@ namespace Press\Uitls\Respond;
 
 
 use Press\Context;
+use React\Http\Response;
+use React\Promise\Promise;
+use React\Promise\PromiseInterface;
 
-function respond(Context $ctx) {
-//    todo:
+function respond(Context $ctx): PromiseInterface
+{
+    return new Promise(function ($resolve, $reject) use ($ctx) {
+        try {
+            $res = $ctx->res;
+
+            $body = $ctx->body;
+            $code = $ctx->status;
+
+            if (!$code) {
+                return $resolve($res);
+            }
+
+            return $resolve($res);
+        } catch (\Exception $error) {
+            return $reject($error);
+        }
+    });
 }
