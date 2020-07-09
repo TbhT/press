685c5c8db207db363a01b5b2fd65dde292b66e4e
TbhT <hustapple132@gmail.com>
2020-07-09 10:53:26 +0800

    feat: update response

--- index.php
+++ index.php
@@ -3,12 +3,3 @@ declare(strict_types=1);
 
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-$app = new \Press\Application();
-
-$app->use(function (\Press\Context $ctx,callable $next) {
-    var_dump('>>>>>>hello world', $ctx->req->getQueryParams());
-    return $next();
-});
-
-$app->listen();
