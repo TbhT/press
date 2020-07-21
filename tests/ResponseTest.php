e73b354e2ae0480a4669b4e9ae2985741292d970
TbhT <hustapple132@gmail.com>
2020-07-21 22:35:53 +0800

    tests: update response body part

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -39,4 +39,96 @@ class ResponseTest extends TestCase
         $res->body = $obj;
         $this->assertSame('application/json; charset=utf-8', $res->get('content-type'));
     }
+
+    /** @test */
+    public function shouldOverrideLength()
+    {
+        $res = $this->createRes();
+
+        $res->type = 'html';
+        $res->body = 'something';
+        $this->assertSame(9, $res->length);
+    }
+
+    /** @test */
+    public function shouldDefaultToTextWhenStringGiven()
+    {
+        $res = $this->createRes();
+        $res->body = 'Tobi';
+        $this->assertSame('text/plain; charset=utf-8', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldSetLengthWhenStringGiven()
+    {
+        $res = $this->createRes();
+        $res->body = 'Tobi';
+        $this->assertSame('4', $res->get('content-length'));
+    }
+
+    /** @test */
+    public function shouldDefaultToTextWhenStringGivenAndNonLeading()
+    {
+        $res = $this->createRes();
+        $res->body = 'aklsdjf < klajsdlfjasd';
+        $this->assertSame('text/plain; charset=utf-8', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldDefaultToHtmlWhenHtmlStringGiven()
+    {
+        $res = $this->createRes();
+        $res->body = '<h1>Tobi</h1>';
+        $this->assertSame('text/html; charset=utf-8', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldSetLengthWhenBodyOverridden()
+    {
+        $res = $this->createRes();
+        $string = '<h1>Tobi</h1>';
+        $res->body = $string;
+        $res->body = $string . $string;
+        $this->assertSame(strlen($res->body), strlen($string) * 2);
+    }
+
+    /** @test */
+    public function shouldDefaultToHtmlWhenLeadingContainsWhitespace()
+    {
+        $res = $this->createRes();
+        $res->body = '    <h1>Tobi</h1>';
+        $this->assertSame('text/html; charset=utf-8', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldDefaultToHtmlWhenXMLGive()
+    {
+        $res = $this->createRes();
+        $res->body = '<?xml version="1.0" encoding="UTF-8"?>\n<俄语>данные</俄语>';
+        $this->assertSame('text/html; charset=utf-8', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldDefaultToStreamWhenStreamGiven()
+    {
+        $res = $this->createRes();
+        $res->body = stream_for('hello');
+        $this->assertSame('application/octet-stream', $res->get('content-type'));
+    }
+
+    /** @test */
+    public function shouldAddErrorHandler()
+    {
+        $this->markTestSkipped('todo add error handler');
+    }
+
+    /** @test */
+    public function shouldDefaultToJSONWhenObjectGiven()
+    {
+        $res = $this->createRes();
+        $result = new stdClass();
+        $result->foo = 'bar';
+        $res->body = $result;
+        $this->assertSame('application/json; charset=utf-8', $res->get('content-type'));
+    }
 }
